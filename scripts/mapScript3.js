969a44ce8e6c7b88917d0b076703a7d028998433
aedean <deanomonkey@hotmail.co.uk>
2017-05-01 18:46:20 +0100

    Update mapScript3.js

--- scripts/mapScript3.js
+++ scripts/mapScript3.js
@@ -10,7 +10,7 @@
           zoom: 10
         });
         
-        directionsDisplay.setMap(map);
+
 
         infoWindow = new google.maps.InfoWindow; //pop up window
         
@@ -21,27 +21,7 @@
 	      		radius: 500,
 		      	type: ['petrol station']
 	     		}, callback);
-		  
-//Get GEOLOCATION
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-			  calculateAndDisplayRoute(directionsService, directionsDisplay);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+		 
           
 	  } //end of initmap function
 
