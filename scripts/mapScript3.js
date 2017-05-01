e967b54df902381fd09e8e9ea4c254669d288bb0
aedean <deanomonkey@hotmail.co.uk>
2017-05-01 19:37:10 +0100

    Update mapScript3.js

--- scripts/mapScript3.js
+++ scripts/mapScript3.js
@@ -67,12 +67,108 @@ var map, infoWindow, directionsService, directionsDisplay, service;
           }
         });
       }
+      
+
+ 
+ NEW CODE 
+ 
+       var map, infoWindow, directionsService, directionsDisplay, service;
+      var pos;
+      function initMap() {
+       directionsService = new google.maps.DirectionsService;
+       directionsDisplay = new google.maps.DirectionsRenderer;
+       var shrewsbury = new google.maps.LatLng(52.711178, -2.756441);	  
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: shrewsbury,
+          zoom: 10
+        });
+        directionsDisplay.setMap(map);
+
+        infoWindow = new google.maps.InfoWindow; //pop up window
+		  
+//Get GEOLOCATION
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+			  calculateAndDisplayRoute(directionsService, directionsDisplay);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+	      
+	  } //end of initmap function
+
+      function callback(results, status) {
+		  if(status == google.maps.places.PlaceServiceStatus.OK){
+			  for (var i = 0; i < results.length; i++){
+				  var place = results[i];
+				  createMarker(results[i]);
+			  }
+		  }
+	  }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+//route function
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: pos,
+          destination: {lat: 52.711178, lng: -2.756441},
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+
+console.log("here");
+
+
 */
 //new
- var map;
-      var infowindow;
 
-      function initMap() {
+var map, 
+    infoWindow, 
+    directionsService, 
+    directionsDisplay, 
+    service,
+    pos;
+
+function initMap() {
+       directionsService = new google.maps.DirectionsService;
+       directionsDisplay = new google.maps.DirectionsRenderer;	  
+
          var shrewsbury = {lat: 52.711178, lng: -2.756441};	
 
         map = new google.maps.Map(document.getElementById('map'), {
@@ -87,7 +183,28 @@ var map, infoWindow, directionsService, directionsDisplay, service;
 	      		location: shrewsbury,
 	      		radius: 500,
 		      	type: ['petrol station']
-	     		}, callback);      
+	     		}, callback);  
+	      
+	//Get GEOLOCATION
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+			  calculateAndDisplayRoute(directionsService, directionsDisplay);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }      
       }
 
       function callback(results, status) {
@@ -108,5 +225,19 @@ var map, infoWindow, directionsService, directionsDisplay, service;
         google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent(place.name);
           infowindow.open(map, this);
+        });
+      }
+//route function
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: pos,
+          destination: {lat: 52.711178, lng: -2.756441},
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
         });
       }
