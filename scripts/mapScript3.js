47b6b86a5fe09fdcea2029961300e5bcc4360d62
aedean <deanomonkey@hotmail.co.uk>
2017-05-01 20:57:58 +0100

    Update mapScript3.js

--- scripts/mapScript3.js
+++ scripts/mapScript3.js
@@ -168,37 +168,52 @@ var map,
 function initMap() {
        directionsService = new google.maps.DirectionsService;
        directionsDisplay = new google.maps.DirectionsRenderer;	  
-       
+       /*
          var shrewsbury = {lat: 52.711178, lng: -2.756441};	
 
         map = new google.maps.Map(document.getElementById('map'), {
           center: shrewsbury,
           zoom: 15
         });
+*/
+	      
+	      
+        infoWindow = new google.maps.InfoWindow;
 
-        infowindow = new google.maps.InfoWindow();
-	      
-	      service = new google.maps.places.PlacesService(map);
-	      service.nearbySearch({
-	      		location: shrewsbury,
-	      		radius: 500,
-		      	type: ['petrol station']
-	     		}, callback);  
-	      
-	//Get GEOLOCATION
-	directionsDisplay.setMap(map);
-	    pos = {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-	calculateAndDisplayRoute(directionsService, directionsDisplay);  
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+		  	      service = new google.maps.places.PlacesService(map);
+	      service.nearbySearch({
+	      		location: pos,
+	      		radius: 500,
+		      	type: ['petrol station']
+	     		}, callback);  
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
       }
 
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
